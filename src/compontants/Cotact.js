b74af6e851dbc90f1419e283bf746d20f80fc1d1
jshenk <jshenk@unleashed-technologies.com>
2020-12-01 12:12:39 -0500

    Test out a netlify form

--- src/compontants/Cotact.js
+++ src/compontants/Cotact.js
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Menu from './Menu';
 import Stanley from '../images/stanley.jpg'
 
 function Contact () {
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+  if ( window.location.search.includes('success=true') ) {
+    setSuccess(true);
+  }
+}, []);
   return (
     <div>
       <div className="mb-6 md:p-6 md:bg-gray-800 md:text-white">
       <Menu />
       </div>
-      <div className="flex container mx-auto mt-10">
-        <div className="md:w-1/3 text-left">
-        <h1 className="mb-4 text-gray-800 text-6xl">Give a Shout</h1>
-        <p className="mb-5 text-xl">My secretary, Stanley, is standing by!</p>
+      <div className="container mx-auto text-left">
+          <h1 className="mb-4 text-gray-800 text-6xl">Give a Shout</h1>
+          <p className="mb-5 text-xl">My secretary, Stanley, is standing by!</p>
+          {success && (
+            <p className="bg-green-200 p-3">
+              Successfully submitted form!
+            </p>
+          )}
+      </div>
+      <div className="flex container mx-auto mt-10 mb-10 shadow p-10">
+        <div className="md:w-1/3 text-left md:pr-10">
         <img src={Stanley} alt="stanley" />
         </div>
 
         <div className="md:w-2/3 text-left">
-          <iframe title="contact me" className="m-auto w-full" src="https://docs.google.com/forms/d/e/1FAIpQLScLddfjqNZw1JPvvWW1KKfI_zj8ml43lMgtyna0ebggyNPJFA/viewform?embedded=true" width="1000" height="1000" frameborder="0" marginheight="0" marginwidth="0" margin="auto">Loading…</iframe>
+        <form name="contact" method="POST">
+          <p>
+            <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" htmlFor="name">Name</label>
+            <input className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner mb-3" type="text" id="name" name="name" />
+          </p>
+          <p>
+            <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" htmlFor="email">Email</label>
+            <input className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner mb-3" type="text" id="email" name="email" />
+          </p>
+          <p>
+            <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" htmlFor="message">Message</label>
+            <textarea className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner px-4 mb-3" id="message" name="message"></textarea>
+          </p>
+          <p>
+            <button className="btn-primary transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-purple-700 hover:bg-purple-900 text-white font-normal py-2 px-4 mr-1 rounded" type="submit" action="/contact?success=true">Send</button>
+          </p>
+        </form>
+                  {/* <iframe title="contact me" className="m-auto w-full" src="https://docs.google.com/forms/d/e/1FAIpQLScLddfjqNZw1JPvvWW1KKfI_zj8ml43lMgtyna0ebggyNPJFA/viewform?embedded=true" width="1000" height="1000" frameborder="0" marginheight="0" marginwidth="0" margin="auto">Loading…</iframe> */}
+
         </div>
       </div>
 
