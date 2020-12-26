47848e30e95b861a0fb12b7772a58a0ca415c4d2
AliMoallem27 <76528174+AliMoallem27@users.noreply.github.com>
2020-12-26 11:34:00 +0330

    reform navbar in mobile view
    navbar was messy in mobile view and in this commit the navbar has been modified.

--- src/compontants/Menu.js
+++ src/compontants/Menu.js
@@ -6,11 +6,11 @@ import React from 'react'
 function Menu (props) {
 
   return (
-    <div className="flex flex-wrap md:flex-no-wrap justify-center md:justify-end md:pr-10 w-full">
-      <NavLink to="/" className="uppercase pr-6" activeClassName="font-medium"><span className="menu-link-hover">Home</span></NavLink>
-      <NavLink to="/work" className="uppercase pr-6" activeClassName="font-medium"><span className="menu-link-hover">Experience</span> </NavLink>
+    <div className="flex md:flex-no-wrap justify-between md:justify-end md:pr-10 w-full">
+      <NavLink to="/" className="uppercase md:pr-6" activeClassName="font-medium"><span className="menu-link-hover">Home</span></NavLink>
+      <NavLink to="/work" className="uppercase md:pr-6" activeClassName="font-medium"><span className="menu-link-hover">Experience</span> </NavLink>
       <NavLink to="/me" className="uppercase md:pr-6" activeClassName="font-medium"><span className="menu-link-hover">Background</span></NavLink>
-      <NavLink to="/contact" className="bg-yellow-500 px-2 uppercase md:bg-transparent md:px-0" activeClassName="font-medium"><span className="menu-link-hover">Contact</span> </NavLink> 
+      <NavLink to="/contact" className="bg-yellow-500 uppercase md:bg-transparent md:px-2 md:px-0" activeClassName="font-medium"><span className="menu-link-hover">Contact</span> </NavLink> 
     </div>
   )
 }
